0d1727dd0ec8e631b6c1910f535200d1acaac3ad
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-07-15 19:35:18 +0300

    fix bugs

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-
 const ctrl = require("../../controllers/contacts");
 
 const { validateBody } = require("../../middlewares");
@@ -10,30 +9,14 @@ const schemas = require("../../schemas/contacts");
 const router = express.Router();
 
 
-
-// router.get('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
 router.get("/", ctrl.listContacts);
 
-// router.get('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-
 router.get("/:id", ctrl.getContactById);
-// router.post('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
-// router.delete('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
+
 router.delete("/:id", ctrl.removeContact);
-// router.put('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
+
 router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
+
 module.exports = router
