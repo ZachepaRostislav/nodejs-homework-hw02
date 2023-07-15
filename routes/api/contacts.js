fb0b525f42eb6282c7c54792abbe7d36b1f2aa60
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-07-15 19:31:05 +0300

    is done ?

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,39 @@
 const express = require('express')
 
-const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../controllers/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody } = require("../../middlewares");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require("../../schemas/contacts");
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
+
+// router.get('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+
+router.get("/", ctrl.listContacts);
+
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+
+
+router.get("/:id", ctrl.getContactById);
+// router.post('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+// router.delete('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+router.delete("/:id", ctrl.removeContact);
+// router.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
 module.exports = router
